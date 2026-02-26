3d6454514636f465db834b4884b37839d4da7d30
JIA123HH-LGTM <2081568163@qq.com>
2026-02-26 22:45:41 +0800

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: 'https://jia123hh.pythonanywhere.com/myApp',
-  timeout: 4000
+  baseURL: 'https://jia123hh.pythonanywhere.com/myApp/',
+  timeout: 10000
 })
 
 export default service
