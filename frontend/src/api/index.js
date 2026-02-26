58b3e09e291bce3c2c46956a5266cd885b8eab49
JIA123HH-LGTM <2081568163@qq.com>
2026-02-26 23:07:33 +0800

    Update base URL for API service

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: 'https://jia123hh.pythonanywhere.com/myApp/',
+  baseURL: 'https://jia123hh.pythonanywhere.com/',
   timeout: 10000
 })
 
